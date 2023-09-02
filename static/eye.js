f003c7adf267bf13e597f163d1cb857d2647254c
Matthew Ronchetto <hello@doamatto.xyz>
2023-09-01 21:08:13 -0700

    fix: conform to normal tab spacing

--- static/eye.js
+++ static/eye.js
@@ -35,46 +35,42 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 30);
   };
 
-  h2Element.addEventListener("mouseover", startAnimation);
-
   startAnimation(); // Trigger the animation when the page first loads
 });
 
 // Eye tracker for cursor
-document.addEventListener('DOMContentLoaded', () => {
-  const pupilContainer = document.getElementById('pupilContainer');
-  const pupilImage = document.querySelector('#pupilContainer img');
+const pupilContainer = document.getElementById('pupilContainer');
+const pupilImage = document.querySelector('#pupilContainer img');
 
-  const movePupil = (mouseX, mouseY) => {
-    const containerRect = pupilContainer.getBoundingClientRect();
-    const pupilImageRect = pupilImage.getBoundingClientRect();
+const movePupil = (mouseX, mouseY) => {
+  const containerRect = pupilContainer.getBoundingClientRect();
+  const pupilImageRect = pupilImage.getBoundingClientRect();
 
-    const containerCenterX = containerRect.left + containerRect.width / 2;
-    const containerCenterY = containerRect.top + containerRect.height / 2;
+  const containerCenterX = containerRect.left + containerRect.width / 2;
+  const containerCenterY = containerRect.top + containerRect.height / 2;
 
-    const deltaX = mouseX - containerCenterX;
-    const deltaY = mouseY - containerCenterY;
+  const deltaX = mouseX - containerCenterX;
+  const deltaY = mouseY - containerCenterY;
 
-    // Calculate the maximum allowed movement range within the container
-    const maxRangeX = (containerRect.width - pupilImageRect.width) / 2;
-    const maxRangeY = (containerRect.height - pupilImageRect.height) / 2;
+  // Calculate the maximum allowed movement range within the container
+  const maxRangeX = (containerRect.width - pupilImageRect.width) / 2;
+  const maxRangeY = (containerRect.height - pupilImageRect.height) / 2;
 
-    // Define a scaling factor to adjust the movement speed
-    const speedScale = 0.05; // Adjust this value to control the speed
+  // Define a scaling factor to adjust the movement speed
+  const speedScale = 0.05; // Adjust this value to control the speed
 
-    // Limit the eye movement within the container with the scaled values
-    const limitedX = Math.max(-maxRangeX, Math.min(deltaX * speedScale, maxRangeX));
-    const limitedY = Math.max(-maxRangeY, Math.min(deltaY * speedScale, maxRangeY));
+  // Limit the eye movement within the container with the scaled values
+  const limitedX = Math.max(-maxRangeX, Math.min(deltaX * speedScale, maxRangeX));
+  const limitedY = Math.max(-maxRangeY, Math.min(deltaY * speedScale, maxRangeY));
 
-    setTimeout(() => {
+  setTimeout(() => {
     pupilImage.style.transform = `translate(${limitedX - 130}px, ${limitedY - 120}px)`;
-    }, 180);
-  };
+  }, 180);
+};
 
-  const handleMouseMove = (e) => {
-    movePupil(e.clientX, e.clientY);
-  };
+const handleMouseMove = (e) => {
+  movePupil(e.clientX, e.clientY);
+};
 
-  document.addEventListener('mousemove', handleMouseMove);
-});
+document.addEventListener('mousemove', handleMouseMove);
   
